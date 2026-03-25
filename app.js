cdef9f53309ba8198b6bf05e74b6aaa4cb2006ff
katsu <sdjgsinhzoghaodg@yandex.ru>
2026-03-25 13:57:18 +0300

    post controller

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const authenticated = require('./middlewares/authenticated');
 const hasRole = require('./middlewares/hasRole');
-const { register, login, getUsers, getRoles, updateUser, deleteUser} = require('./controllers/user');
+const { register, login, getUsers, getRoles, updateUser, deleteUser } = require('./controllers/user');
+const { getPosts, getPost, addPost, editPost, deletePost} = require('./controllers/post');
 const mapUser = require('./helpers/mapUser');
+const mapPost = require('./helpers/mapPost');
 const ROLES = require('./constants/roles');
 
 const port = 3001;
@@ -41,7 +43,49 @@ app.post('/logout', (req, res) => {
 		.send({})
 });
 
+app.get('/posts', async (req, res) => {
+	const { posts, lastPage } = await getPosts(
+		req.query.search,
+		req.query.limit,
+		req.query.page
+	);
+	
+	res.send({ data: { posts: posts.map(mapPost), lastPage } });
+});
+
+app.get('/posts/:id', async (req, res) => {
+	const post = await getPost(req.params.id);
+	
+	res.send({ data: mapPost(post) });
+});
+
 app.use(authenticated);
+
+app.post('/posts', hasRole([ROLES.ADMIN]), async (req, res) => {
+	const newPost = await addPost({
+		title: req.body.title,
+		content: req.body.content,
+		image: req.body.imageUrl,
+	});
+	
+	res.send({ data: mapPost(newPost) });
+});
+
+app.patch('/posts/:id', hasRole([ROLES.ADMIN]), async (req, res) => {
+	const updatedPost = await editPost(req.params.id, {
+		title: req.body.title,
+		content: req.body.content,
+		image: req.body.imageUrl,
+	});
+	
+	res.send({ data: mapPost(updatedPost) });
+});
+
+app.delete('/posts/:id', hasRole([ROLES.ADMIN]), async (req, res) => {
+	await deletePost(req.params.id);
+	
+	res.send({ error: null });
+})
 
 app.get('/users', hasRole([ROLES.ADMIN]), async (req, res) => {
 	const users = await getUsers();
--- controllers/post.js
+++ controllers/post.js
@@ -1,5 +1,42 @@
-// add
-// edit
-// delete
-// get w search & pagination
-// get 1
+const Post = require('../models/Post');
+
+function addPost(post) {
+	return Post.create(post);
+}
+
+async function editPost(id, post) {
+	const newPost = await Post.findByIdAndUpdate(id, post, { returnDocument: 'after' });
+	
+	return newPost;
+}
+
+function deletePost(id) {
+	return Post.deleteOne({ _id: id });
+}
+
+async function getPosts(search = '', limit = 9, page = 1) {
+	const [posts, count] = await Promise.all([
+		Post.find({ title: { $regex: search, $options: 'i' } })
+			.limit(limit)
+			.skip((page -1) * limit)
+			.sort({ createdAt: -1 }),
+		Post.countDocuments({ title: { $regex: search, $options: 'i' } })
+	])
+	
+	return {
+		posts,
+		lastPage: Math.ceil(count / limit),
+	}
+}
+
+function getPost(id) {
+	return Post.findById(id);
+}
+
+module.exports = {
+	addPost,
+	editPost,
+	deletePost,
+	getPosts,
+	getPost
+}
